f9a68c11d0073c057defed618762467db99b8838
sonphan18071999 <43442285+sonphan18071999@users.noreply.github.com>
2020-07-09 20:29:53 +0700

    Stablity version API logins

---
+++ RapPhimOOSD/RapPhimOOSD/Models/DatabaseDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RapPhimOOSD.Models
+{
+    public class DatabaseDbContext:DbContext
+    {
+        public DatabaseDbContext(DbContextOptions options): base(options)
+        {
+        }
+        public DbSet<Login> Logins { get; set; }
+       
+    }
+}
